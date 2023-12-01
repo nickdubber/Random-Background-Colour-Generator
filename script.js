804ed7d30e39f048c1cfe7d2c0b1784ee7b89f82
nickdubber <nicholasdubber@gmail.com>
2023-12-01 20:55:37 +0000

    Updates

---
+++ script.js
@@ -0,0 +1,23 @@
+function test()
+    if("buttonPush"){
+        console.log("Works")
+    }
+
+
+document.getElementById('buttonPush').onclick = function(){
+    alert("Clicked");
+}
+// Listen for button push
+
+
+//react on button push to show its pushed
+
+
+//if button pushed, initiate randomizer function
+
+
+//pick out values from an array in format 
+
+
+
+//deploy randomized colour to CSS
